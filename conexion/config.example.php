edcc9258c51144cfa9ea8e180eef62c7a091d3ad
Eva-Contreras <evacontreras425@gmail.com>
2025-10-22 20:30:29 -0500

    b

---
+++ conexion/config.example.php
@@ -0,0 +1,9 @@
+<?php
+// Copia este archivo como config.php y actualiza tus datos locales
+// Cada compañero deberá copiarlo y renombrarlo como config.php y poner sus credenciales reales.
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "cafeteria_java";
+?>
